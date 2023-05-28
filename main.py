25ded78a9a59038e013efb30d719d6b7962f5203
Nisarg <202003030@daiict.ac.in>
2023-05-28 15:04:59 +0530

    Update main:app

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "Hello World"}
+
+@app.get("/blog/{id}")
+def show(id: int):
+    # fetch blog with id = id
+    return {"data": id}
